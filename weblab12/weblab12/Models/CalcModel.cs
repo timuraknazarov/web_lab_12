b148a4b4dee442fd9475c7fb3234d8bebc91953b
Timur <rocketridestudio@gmail.com>
2023-01-27 19:37:26 +0700

    lab 12 done

---
+++ weblab12/weblab12/Models/CalcModel.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2._2.Models
+{
+    public class CalcModel
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public string Operation { get; set; }
+
+        public string Calc()
+        {
+            return Operation switch
+            {
+                "+" => $"{x} + {y} = {x + y}",
+                "-" => $"{x} - {y} = {x - y}",
+                "*" => $"{x} * {y} = {x * y}",
+                "/" => y != 0 ? $"{x} /  {y} = {x / y}" : "Division by zero",
+                _ => "Invalid operation"
+            };
+        }
+    }
+}
